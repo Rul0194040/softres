625b19735c5864f0be5f5b6c3d1d59c0c910ad5a
014411172 <014411172@ulsaoaxaca.edu.mx>
2021-08-15 15:35:06 -0500

    CRUD insumo

--- src/insumo/insumo.controller.ts
+++ src/insumo/insumo.controller.ts
@@ -1,8 +1,9 @@
-import { getRepository, UpdateDateColumn } from 'typeorm';
+import { DeleteResult, getRepository, UpdateResult } from 'typeorm';
 import { InsumoEntity } from '@softres/insumo/insumo.entity';
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
@@ -11,26 +12,38 @@ import {
 } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { CreateInsumoDTO } from './DTO/create-insumo.dto';
+import { UpdateInsumoDTO } from './DTO/update-insumo.dto';
+import { InsumoService } from './insumo.service';
 
 @Controller('insumo')
 export class InsumoController {
+  constructor(private readonly insumoService: InsumoService) {}
+
   @Post()
   async createInsumo(@Body() insumo: CreateInsumoDTO): Promise<InsumoEntity> {
     const insumoToCreate = plainToClass(InsumoEntity, insumo);
+    console.log(insumoToCreate);
     const created = await getRepository(InsumoEntity).save(insumoToCreate);
     return created;
   }
 
   @Get(':id')
   async GetInsumoById(
-    @Param('insumoId', ParseIntPipe) insumoId: number,
+    @Param('id', ParseIntPipe) id: number,
   ): Promise<InsumoEntity> {
-    return await getRepository(InsumoEntity).findOne(insumoId);
+    return await getRepository(InsumoEntity).findOne(id);
   }
 
   @Put(':id')
   async updateInsumo(
-    @Param('insumoId', ParseIntPipe) insumoId: number,
-    @Body() insumo: CreateInsumoDTO,
-  ): Promise<InsumoEntity> {}
+    @Param('id', ParseIntPipe) id: number,
+    @Body() insumo: UpdateInsumoDTO,
+  ): Promise<UpdateResult> {
+    return await getRepository(InsumoEntity).update(id, insumo);
+  }
+
+  @Delete(':id')
+  async deleteInsumo(@Param('id') id: number): Promise<DeleteResult> {
+    return await getRepository(InsumoEntity).delete(id);
+  }
 }
