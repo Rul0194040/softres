214665798d72617b4536bb88185de2383f9a83bf
Raul Sierra Vasconcelos <rsierravasconcelos@gmail.com>
2021-09-08 12:48:21 -0500

    imports que faltaban

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,6 +27,11 @@ import { VentasModule } from './ventas/ventas.module';
 import { CompraEntity } from './compra/entities/compra.entity';
 import { CompraDetalleEntity } from './compra/entities/compraDetalles.entity';
 import { CompraSolicitudEntity } from './compra/entities/solicitudCompra.entity';
+import { AlmacenModule } from './almacen/almacen.module';
+import { AlmacenEntity } from './almacen/entitys/almacen.entity';
+import { AlmacenDetalleEntity } from './almacen/entitys/almacenDetalle.entity';
+import { AppConfig } from './app.config';
+import { CotizacionModule } from './cotizacion/cotizacion.module';
 
 @Module({
   imports: [
